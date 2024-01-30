a5ea9cd82e906d3468c62a7f3cd6ff5a7a66a83f
Paul M Shumate <+>
2024-01-30 16:20:02 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInFeet(10, 20, 30, 40);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(destination - start);
+    return blocksTravelled * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const feetTravelled = distanceTraveledInFeet
+}
