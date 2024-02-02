3cc291ba86837da67044c225b5b452985af1dd6c
Paul M Shumate <+>
2024-02-02 15:31:43 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-function distanceFromHqInBlocks(someValue) {
-  return Math.abs(someValue - 42);
-}
-
-function distanceFromHqInFeet(someValue) {
-  return distanceFromHqInFeet(10, 20, 30, 40);
-}
-
-function distanceTravelledInFeet(start, destination) {
-    const blocksTravelled = Math.abs(destination - start);
-    return blocksTravelled * 264;
-}
-
-function calculatesFarePrice(start, destination) {
-  const feetTravelled = distanceTraveledInFeet
-}
+function distanceFromHqInBlocks(location) {
+    const hqLocation = 42;
+    return Math.abs(location - hqLocation);
+  }
+  
+  function distanceFromHqInFeet(location) {
+    const blocks = distanceFromHqInBlocks(location);
+    return blocks * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const distance = Math.abs(destination - start);
+    return distance * 264; 
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
